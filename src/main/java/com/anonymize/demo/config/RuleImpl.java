74a12a4559dc62c19026294630480b7c3ed76910
PRANJAL <pranjal0706@gmail.com>
2024-09-21 21:19:40 +0530

    added test data

--- src/main/java/com/anonymize/demo/config/RuleImpl.java
+++ src/main/java/com/anonymize/demo/config/RuleImpl.java
@@ -4,6 +4,13 @@ import com.anonymize.demo.model.Method;
 import com.anonymize.demo.model.Rule;
 import com.anonymize.demo.service.DynamicDatabaseService;
 
+
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Supplier;
+
+
 import javax.sql.DataSource;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -15,73 +22,47 @@ import static javax.swing.UIManager.getInt;
 
 public class RuleImpl {
 
+    private final Faker faker = new Faker();
 
     public void applyRules(DataSource dataSource, HashMap<Rule, Method> ruleMethodMap) throws SQLException {
-
-        for (Map.Entry<Rule, Method> rule : ruleMethodMap.entrySet()){
-            String sql = "select " + rule.getKey().getPk_column_name() + " from " + rule.getKey().getTable_name();
+        for (Map.Entry<Rule, Method> entry : ruleMethodMap.entrySet()) {
+            Rule rule = entry.getKey();
+            Method method = entry.getValue();
+            String sql = "SELECT " + rule.getPk_column_name() + " FROM " + rule.getTable_name();
 
             DynamicDatabaseService dynamicDatabaseService = new DynamicDatabaseService(dataSource);
-            //int cnt = ((Long)  dynamicDatabaseService.queryForList(sql).get(0).entrySet().iterator().next().getValue()).intValue();
-            for (Object primary_key : dynamicDatabaseService.queryForList(sql)){
-                String pk = primary_key.toString();
-                //int col_siz=dataSource.getConnection().getMetaData().getColumns(null,null,rule.getKey().getTable_name(),rule.getKey().getColumn_name()).getInt("COLUMN_SIZE");
-                //System.out.println("col_siz-->"+col_siz);
-            //getConnection().getMetaData().getColumns(null,null,rule.getKey().getTable_name(),rule.getKey().getColumn_name())
-
-                Faker faker = new Faker();
-                if (rule.getValue().getAnon_method_name().equalsIgnoreCase("Random_String")) {
-                    // faker.getRamdomString(cnt);
-                    String updateSql = "update " + rule.getKey().getTable_name()
-                            + " set " + rule.getKey().getColumn_name()
-                            + " = '" + faker.random().hex(10) + "'" +
-                            " where " + rule.getKey().getPk_column_name()
-                            + " = " + "'" + pk + "';" ; // get value from the map
-                    System.out.println(updateSql);
-                    dynamicDatabaseService.updateQuery(updateSql);
-                }else if(rule.getValue().getAnon_method_name().equalsIgnoreCase("Random_Name")){
-
-                    String updateSql = "update " + rule.getKey().getTable_name()
-                            + " set " + rule.getKey().getColumn_name()
-                            + " = '" + faker.name().fullName().replace("'","")+ "'" +
-                            " where " + rule.getKey().getPk_column_name()
-                            + " = " + "'" + pk + "';" ; // get value from the map
-                    System.out.println(updateSql);
-                    dynamicDatabaseService.updateQuery(updateSql);
-                }else if(rule.getValue().getAnon_method_name().equalsIgnoreCase("Random_Age")){
-
-                    String updateSql = "update " + rule.getKey().getTable_name()
-                            + " set " + rule.getKey().getColumn_name()
-                            + " = '" + faker.number().numberBetween(10,80)+ "'" +
-                            " where " + rule.getKey().getPk_column_name()
-                            + " = " + "'" + pk + "';" ; // get value from the map
-                    System.out.println(updateSql);
-                    dynamicDatabaseService.updateQuery(updateSql);
-                }else if(rule.getValue().getAnon_method_name().equalsIgnoreCase("Random_Phone")){
-
-                    String updateSql = "update " + rule.getKey().getTable_name()
-                            + " set " + rule.getKey().getColumn_name()
-                            + " = '" + faker.phoneNumber().phoneNumber()+ "'" +
-                            " where " + rule.getKey().getPk_column_name()
-                            + " = " + "'" + pk + "';" ; // get value from the map
-                    System.out.println(updateSql);
-                    dynamicDatabaseService.updateQuery(updateSql);
-                }else if(rule.getValue().getAnon_method_name().equalsIgnoreCase("Random_Address")){
-
-                    String updateSql = "update " + rule.getKey().getTable_name()
-                            + " set " + rule.getKey().getColumn_name()
-                            + " = '" + faker.address().fullAddress()+ "'" +
-                            " where " + rule.getKey().getPk_column_name()
-                            + " = " + "'" + pk + "';" ; // get value from the map
-                    System.out.println(updateSql);
-                    dynamicDatabaseService.updateQuery(updateSql);
-                }
+            for (Object primaryKey : dynamicDatabaseService.queryForList(sql)) {
+                String pk = primaryKey.toString();
+                String updateSql = generateUpdateSql(rule, method, pk);
+                System.out.println(updateSql);
+                dynamicDatabaseService.updateQuery(updateSql);
             }
-
-
-
         }
     }
 
+    private String generateUpdateSql(Rule rule, Method method, String pk) {
+        String columnName = rule.getColumn_name();
+        String pkColumnName = rule.getPk_column_name();
+        String newValue = generateNewValue(method);
 
+        return String.format("UPDATE %s SET %s = '%s' WHERE %s = '%s';",
+                rule.getTable_name(), columnName, newValue, pkColumnName, pk);
+    }
+
+    private String generateNewValue(Method method) {
+        switch (method.getAnon_method_name().toLowerCase()) {
+            case "random_string":
+                return faker.random().hex(10);
+            case "random_name":
+                return faker.name().fullName().replace("'", "");
+            case "random_age":
+                return String.valueOf(faker.number().numberBetween(10, 80));
+            case "random_phone":
+                return faker.phoneNumber().phoneNumber();
+            case "random_address":
+                return faker.address().fullAddress().replace("'", "");
+            default:
+                throw new IllegalArgumentException("Unknown anonymization method: " + method.getAnon_method_name());
+        }
+    }
 }
